fb093a97eb64b03bdcd06d3cd3db53e17077c4a2
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 00:36:10 +0200

    make-release.sh bug fix

--- make-release.sh
+++ make-release.sh
@@ -21,7 +21,7 @@ repos_dir=../lua-files-git/git-repos # relative to release_dir
 packages="$(./packages.sh)"
 cd "$release_dir"
 for package in $packages; do
-	[ "$(git remote | grep $package)" ] || {
+	[ "$(git remote | grep ^$package\$)" ] || {
 		git remote add $package \
 			"$repos_dir/$package" #ssh://git@github.com/capr/$package
 	}
