562415d7e077088e2d2611dbd9b297bef29d28b4
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2013-12-25 01:02:34 +0200

    upx should be in PATH

--- upx-all.sh
+++ upx-all.sh
@@ -1,6 +1,5 @@
 # compress all windows binaries
 # TIP: get upx from http://upx.sourceforge.net/download/upx309w.zip
 
-upx="$(cd ..; pwd)/upx.exe"
 cd ../lua-files/bin/mingw32
-"$upx" -q *.dll *.exe lanes/*.dll
+upx -q *.dll *.exe lanes/*.dll
