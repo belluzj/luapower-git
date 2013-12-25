a50789f5aafd551937324cc8504a48d7ea3466bf
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2013-12-26 01:50:44 +0200

    gitp doc

--- gitp.sh
+++ gitp.sh
@@ -1,2 +1,5 @@
+# git wrapper that can be called from a git-dir but which
+# actually executes git from the project's work-tree with --git-dir option.
+
 gitdir="$PWD"
 cd ../.. && git --git-dir="$gitdir/.git" "$@"
