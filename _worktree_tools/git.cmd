d1172170db4cfa6b27731e84ec5c3a84af044371
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-04 18:42:22 +0200

    unit project; refactored worktree tools

--- _worktree_tools/git.cmd
+++ _worktree_tools/git.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem find git.exe and run it with --git-dir
+rem find git.exe in PATH and run it with --git-dir
 
 if "%PROJECT%"=="" goto :no_project
 for %%i in (git.exe) do call %%~$PATH:i --git-dir=_git\%PROJECT%\.git %*
@@ -8,7 +8,7 @@ goto done
 :no_project
 echo not tracking any project.
 echo type ^`proj ^<project^>^` first to track a project.
-echo type ^`proj^` to list all projects
+echo type ^`proj^` to list all projects.
 goto done
 
 :done
--- _worktree_tools/proj.cmd
+++ _worktree_tools/proj.cmd
@@ -5,6 +5,8 @@ if "%1"=="" goto list_projects else goto set_project
 
 :set_project
 set PROJECT=%1
+rem we could have used `set GIT_DIR=%~dp0/_git/%PROJECT%/.git`
+rem and removed the git wrapper but that doesn't work with msys-git 1.8.4 (maybe in the future).
 echo tracking %PROJECT%
 echo ------------------
 prompt [%PROJECT%] $P$G
