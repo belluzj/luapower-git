1f4da855d855780bd857f034044de4d9fe9d1d1f
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-06 19:49:55 +0200

    build script for all packages

---
+++ build-all.sh
@@ -0,0 +1,59 @@
+# build all C packages in order
+set -e
+
+[ "$OSTYPE" == "msys" ] && platform=mingw32 || platform=linux32
+cd ..
+
+indep_packages="
+luajit
+chipmunk
+clipper
+fribidi
+genx
+giflib
+hpdf
+hunspell
+libb64
+libexif
+libjpeg
+libunibreak
+md5
+minizip
+nanojpeg
+pixman
+pmurhash
+sha2
+ucdn
+zlib
+"
+
+luajit_packages="
+lfs
+lpeg
+cjson
+socket
+struct
+vararg
+lanes
+wluajit
+"
+
+zlib_packages="
+libpng
+freetype
+cairo
+"
+
+ucdn_packages="
+harfbuzz
+"
+
+packages="$indep_packages $luajit_packages $zlib_packages $ucdn_packages"
+
+for package in $packages; do
+	[ -f "csrc/$package/build-$platform.sh" ] && {
+		cd "csrc/$package"
+		./build-$platform.sh
+		cd ../..
+	}
+done
