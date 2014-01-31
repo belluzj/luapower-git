4d2a9cc964d0cc00f3766b0851e85d6e2fda438d
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-31 21:34:51 +0200

    platform.sh now detects osx

--- csrc/platform.sh
+++ csrc/platform.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # detect current platform
-[ "$OSTYPE" = "msys" ] && platform=mingw32 || [ "$(uname -m)" == "x86_64" ] && platform=linux64 || platform=linux32
+[ "$OSTYPE" = "msys" ] && platform=mingw32 || {
+  [ "${OSTYPE#darwin}" != "$OSTYPE" ] && platform=osx64 || {
+    [ "$(uname -m)" = "x86_64" ] && platform=linux64 || platform=linux32
+  }
+}
 echo $platform
---
+++ replace-origin.sh
@@ -0,0 +1,2 @@
+echo $PROJECT
+git remote set-url origin "ssh://git@github.com/luapower/$PROJECT"
